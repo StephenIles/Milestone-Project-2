4b430db8090b3b0aa75d24fdeba591bb32ef371c
StephenIles <stephenciles@gmail.com>
2024-05-21 19:40:09 +0100

    Added animation to the cookie when its clicked for a visual feedback

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -59,17 +59,14 @@ $(function () {
   $("#cookie").on("click", function () {
     currentCookies += cookiesPerClick;
     $("#points").text(currentCookies);
+    $("#cookie").css("transform", "scale(1.05, 1.05");
+    setTimeout(function() {
+      $("#cookie").css("transform", "scale(1, 1)");
+    }, 50);
   });
 
   // Button Functionallity
   $.each(upgradeButtons, function (index, value) {
-    $(upgradeButtons[index]).on("mouseenter", function () {
-      $(upgradeButtons[index]).css("background-color", "#003285")
-    });
-
-    $(upgradeButtons[index]).on("mouseleave", function () {
-      $(upgradeButtons[index]).css("background-color", "#2A629A")
-    });
 
     $(value).on("click", function () {
       if (currentCookies > upgradePrices[index]) {
