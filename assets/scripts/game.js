e2c9f02b9d34c64ab0b99fba665f3c8cbeb2c615
StephenIles <stephenciles@gmail.com>
2024-05-21 20:32:51 +0100

    Updates the Variables to use const and let instead

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,10 +1,10 @@
 $(function () {
-  var currentCookies = 0;
-  var cookiesPerClick = 1;
-  var cookiesPerSecond = 0;
-  var cookiesPerSecondAddition = 1;
-  var upgradeLevels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  var upgradeLevelsIds = [
+  let currentCookies = 0;
+  let cookiesPerClick = 1;
+  let cookiesPerSecond = 0;
+  let cookiesPerSecondAddition = 1;
+  let upgradeLevels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const upgradeLevelsIds = [
     "#u1l",
     "#u2l",
     "#u3l",
@@ -14,9 +14,9 @@ $(function () {
     "#u7l",
     "#u8l",
     "#u9l",
-    "#u10l",
+    "#u10l"
   ];
-  var upgradeMaxIds = [
+  const upgradeMaxIds = [
     "#u1max",
     "#u2max",
     "#u3max",
@@ -26,12 +26,12 @@ $(function () {
     "#u7max",
     "#u8max",
     "#u9max",
-    "#u10max",
+    "#u10max"
   ];
-  var upgradePrices = [
+  let upgradePrices = [
     10, 250, 1500, 3000, 5000, 7500, 12000, 18000, 30000, 50000,
   ];
-  var upgradePricesIds = [
+  const upgradePricesIds = [
     "#u1p",
     "#u2p",
     "#u3p",
@@ -43,7 +43,7 @@ $(function () {
     "#u9p",
     "#u10p",
   ];
-  var upgradeButtons = [
+  const upgradeButtons = [
     ".b1",
     ".b2",
     ".b3",
@@ -60,14 +60,13 @@ $(function () {
     currentCookies += cookiesPerClick;
     $("#points").text(currentCookies);
     $("#cookie").css("transform", "scale(1.05, 1.05");
-    setTimeout(function() {
+    setTimeout(function () {
       $("#cookie").css("transform", "scale(1, 1)");
     }, 50);
   });
 
   // Button Functionallity
   $.each(upgradeButtons, function (index, value) {
-
     $(value).on("click", function () {
       if (currentCookies > upgradePrices[index]) {
         if (upgradeLevels[index] < 9) {
@@ -79,7 +78,7 @@ $(function () {
           cookiesPerClick += 10;
           $(upgradeLevelsIds[index]).text(upgradeLevels[index]);
           $(upgradePricesIds[index]).text(upgradePrices[index]);
-        } else  if (upgradeLevels[index] == 9) {
+        } else if (upgradeLevels[index] == 9) {
           upgradeLevels[index] += 1;
           $(upgradeLevelsIds[index]).text(upgradeLevels[index]);
           $(upgradeMaxIds[index]).show();
@@ -91,13 +90,13 @@ $(function () {
 
   // Reset Button functionality
   $(".reset-button").on("click", function () {
-    var top1 = $("#top1").text();
-    var top2 = $("#top2").text();
-    var top3 = $("#top3").text();
-    var top4 = $("#top4").text();
-    var top5 = $("#top5").text();
+    const top1 = $("#top1").text();
+    const top2 = $("#top2").text();
+    const top3 = $("#top3").text();
+    const top4 = $("#top4").text();
+    const top5 = $("#top5").text();
 
-    if(currentCookies > top1) {
+    if (currentCookies > top1) {
       $("#top1").text(currentCookies);
       $("#top2").text(top1);
       $("#top3").text(top2);
@@ -117,8 +116,6 @@ $(function () {
       $("#top5").text(top4);
     } else if (currentCookies > top5) {
       $("#top5").text(currentCookies);
-    } else {
-
     }
 
     currentCookies = 0;
@@ -126,9 +123,11 @@ $(function () {
     cookiesPerSecond = 0;
     cookiesPerSecondAddition = 1;
     upgradeLevels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    upgradePrices = [10, 250, 1500, 3000, 5000, 7500, 12000, 18000, 30000, 50000];
+    upgradePrices = [
+      10, 250, 1500, 3000, 5000, 7500, 12000, 18000, 30000, 50000,
+    ];
 
-    $.each(upgradeLevels, function(index, value) {
+    $.each(upgradeLevels, function (index, value) {
       $(upgradeLevelsIds[index]).text(upgradeLevels[index]);
       $(upgradePricesIds[index]).text(upgradePrices[index]);
       $("#points").text(currentCookies);
@@ -148,14 +147,14 @@ $(function () {
   });
 
   // Functionallity for automatic cookies
-  setInterval(()=> {
+  setInterval(() => {
     currentCookies += cookiesPerSecond;
     $("#cps").text(cookiesPerSecond);
     $("#points").text(currentCookies);
   }, 1000);
 
-  setInterval(()=> {
-    $.each(upgradeButtons, function(index, value) {
+  setInterval(() => {
+    $.each(upgradeButtons, function (index, value) {
       if (currentCookies > upgradePrices[index]) {
         if (upgradeLevels[index] != 10) {
           $(value).css("background-color", "#7AB2B2");
@@ -165,7 +164,6 @@ $(function () {
       } else {
         $(value).css("background-color", "#A9A9A9");
       }
-    })
-  })
-
+    });
+  });
 });
